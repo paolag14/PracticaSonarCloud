cb76963d444c9f00d7ecd91c15cf0ed5158a4b7b
Paola Gómez <90631352+paolag14@users.noreply.github.com>
2023-12-13 14:22:39 +0100

    Update prueba.java

--- prueba.java
+++ prueba.java
@@ -1,5 +1,13 @@
-public class prueba{
-  public static void main(){
-    System.out.println("Hola mundo");
-  }
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyPrueba {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyPrueba.class);
+
+    private MyPrueba() { }
+
+    public static void main(String[] args) { logger.info("Hola mundo"); }
 }
